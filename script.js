bc32ce220c8f66d8d2161030f7e04882480abac9
Vvalenkah <vano-sai@tut.by>
2022-05-12 20:27:51 +0300

    feat: js and meme files added (чт 12.05.22 20:28:01)

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-elasd
+let offset = 0;
+const sliderLine = document.querySelector('.slider-track');
+const dots = document.querySelectorAll('.dot-container');
+const dot = document.querySelectorAll('.dot');
+let slideCount = 1;
+const slideStep = 256;
+
+
+
+dots.forEach(element => element.addEventListener('click', () => {
+    let idNumber = Number(element.id.slice(4));
+    console.log(idNumber);
+    if (idNumber === slideCount) {
+        return;
+    }
+    if (idNumber < slideCount) {
+        moveLeft(idNumber);
+    }
+    if (idNumber > slideCount) {
+        moveRight(idNumber);
+    }
+    slideCount = idNumber;
+    removeActive();
+    element.lastElementChild.classList.add('active');
+}));
+
+dots.forEach(element => element.addEventListener('mouseover', () => {
+    element.lastElementChild.classList.add('hover-active');
+}));
+
+dots.forEach(element => element.addEventListener('mouseout', () => {
+    element.lastElementChild.classList.remove('hover-active');
+}));
+
+function moveLeft(value) {
+    offset -= slideStep * (slideCount - value);
+    sliderLine.style.left = -offset + 'px';
+
+}
+
+function moveRight(value) {
+    offset += slideStep * (value - slideCount);;
+    sliderLine.style.left = -offset + 'px';
+
+}
+
+function removeActive() {
+    dots.forEach(element => element.lastElementChild.classList.remove('active'));
+}
+
+// dots.forEach(element => element.onmouseover = function() {
+//     element.classList.add('active');
+// })
